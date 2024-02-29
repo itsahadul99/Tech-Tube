58659a5d605576cbd433e25a23b2cfffd80a7b3a
itsahadul99 <itsahadul999@gmail.com>
2024-02-29 16:40:25 +0600

    complete card section

--- js/tech-tube.js
+++ js/tech-tube.js
@@ -1,4 +1,6 @@
 const allBtnContainer = document.getElementById('all-btn-container');
+const cardContainer = document.getElementById('card-container');
+const emptyContainer = document.getElementById('empty-btn-container');
 const loadAllCategoryData = async () => {
     const res = await fetch(` https://openapi.programming-hero.com/api/videos/categories`);
     const data = await res.json();
@@ -11,8 +13,49 @@ const loadAllCategoryData = async () => {
         allBtn.innerText = `
             ${categoriesName.category}
         `
+        allBtn.addEventListener("click", () => loadAllId(categoriesName.category_id
+        )
+
+        );
         allBtnContainer.appendChild(allBtn);
     });
 
 }
+const loadAllId = async (id) => {
+    cardContainer.innerHTML = '';
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const data = await res.json();
+    const allItem = data.data;
+    if(allItem.length === 0){
+        emptyContainer.classList.remove('hidden');
+    }
+    else{
+        emptyContainer.classList.add('hidden')
+    }
+    // console.log(allItem);
+    for(const item of allItem){
+        console.log(item);
+        const card = document.createElement('div');
+        card.className = `space-y-5`
+        card.innerHTML = `
+        <div class="w-full"><img class = "w-full" class="" src="${item.thumbnail}" alt=""></div>
+        <div class="flex gap-5">
+            <div>
+                <img class = "rounded-full w-20 h-16" src="${item.authors[0].profile_picture}" alt="">
+            </div>
+            <div>
+                <h1 class = "font-bold text-xl">${item.title}</h1>
+                <div class="flex gap-5 opacity-80">
+                    <p class = "opacity-80 text-[#252525B3]">${item.authors[0].profile_name}</p>
+                    <img src="./logos/fi_10629607.png" alt="">
+                </div>
+                <p class = "opacity-80 text-[#252525B3]">${item.others.views}</p>
+            </div>
+
+        </div>
+        `
+        cardContainer.appendChild(card);
+    }
+}
+
 loadAllCategoryData()
